0bbc6ba2bf31d4bf2f56c90f7c3fcbbead36d035
Upyouth - Tech Startup Ecosystem <127721216+upyouthtech@users.noreply.github.com>
2025-03-11 13:10:05 +0000

    page titles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,7 +15,7 @@
 </template>
 <script setup lang="ts">
 definePageMeta({
-  title: 'Homepage - UpYouth - Tech Startup Ecosystem',
+  title: 'UpYouth - Tech Startup Ecosystem',
   meta: [
     { name: 'description', content: 'We are a youth-focused tech startup ecosystem providing the best resources for Vietnamese entrepreneurs to find REAL ventures.' }
   ]
